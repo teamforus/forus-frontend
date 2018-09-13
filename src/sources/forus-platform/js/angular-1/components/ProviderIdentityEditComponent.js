d43a1199e656495ab29c83fb7f5ef083aee14ada
dev-rminds <dev@rminds.nl>
2018-09-13 12:34:54 +0300

    provider dashboard identities

---
+++ src/sources/forus-platform/js/angular-1/components/ProviderIdentityEditComponent.js
@@ -0,0 +1,56 @@
+let ProviderIdentityEditComponent = function(
+    $state,
+    $stateParams,
+    ProviderIdentityService,
+    FormBuilderService
+) {
+    let $ctrl = this;
+
+    $ctrl.$onInit = function() {
+        let values = ProviderIdentityService.apiResourceToForm(
+            $ctrl.providerIdentity || {}
+        );
+
+        $ctrl.form = FormBuilderService.build(values, (form) => {
+            let promise;
+
+            form.lock();
+
+            if ($ctrl.providerIdentity) {
+                promise = ProviderIdentityService.update(
+                    $stateParams.organization_id,
+                    $stateParams.id,
+                    form.values
+                )
+            } else {
+                promise = ProviderIdentityService.store(
+                    $stateParams.organization_id,
+                    form.values
+                )
+            }
+
+            promise.then((res) => {
+                $state.go('provider-identities', {
+                    organization_id: $stateParams.organization_id
+                });
+            }, (res) => {
+                form.errors = res.data.errors;
+                form.unlock();
+            });
+        });
+    };
+};
+
+module.exports = {
+    bindings: {
+        providerIdentity: '<'
+    },
+    controller: [
+        '$state', 
+        '$stateParams', 
+        'ProviderIdentityService', 
+        'FormBuilderService', 
+        ProviderIdentityEditComponent
+    ],
+    templateUrl: 'assets/tpl/pages/provider-identity-edit.html'
+};
