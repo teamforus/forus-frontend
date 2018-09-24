37e5c4d51b6ab858f68df0ae0de99cb4d5fe58a4
GerbenB <gerben@forus.io>
2018-09-24 08:55:40 +0200

    translations
    some translations

--- src/sources/forus-webshop/js/angular-1/i18n/i18n-en.js
+++ src/sources/forus-webshop/js/angular-1/i18n/i18n-en.js
@@ -3,11 +3,53 @@ module.exports = {
         en: 'English',
         nl: 'Dutch',
     },
+    topnavbar: {
+        items: {
+            funds:"FONDSEN",
+            products:"PRODUCTEN",
+            identity:"Identiteit",
+            buttons: {
+                activate:"Activatiecode",
+                login:"Login",
+                voucher:"Mijn vouchers",
+                authorize:"Authoriseer apparaat",
+                logout:"Uitloggen",
+                products:"Producten",
+            }
+        }
+    },
+
     home: {
         header: {
             title: "Generieke webshop voor gemeentelijke regelingen",
             subtitle: "Op deze webshop kunt u zich aanmelden voor een fonds, waarna u met een voucher langs winkels, sportverenigingen en/of culturele instellingen kunt gaan.",
             button: 'LEES MEER OVER HOE HET SYSTEEM WERKT',
+        },
+        map: {
+            title: "Overzicht van alle leveranciers",
+            subtitle: "Binnen het forus platform werken sponsoren en leveranciers samen om de beste dienst te leveren voor het publieke domein.",
+            button: 'BEKIJK DE KAART',
+        }
+    },
+    products: {
+        header: {
+            title: "Aanbiedingen",
+            select: "Selecteer categorie...",
+            search: "Zoek naar een aanbieding",
+        }
+    },
+    empty: {
+        header: {
+            title: "Er zijn geen aanbiedingen",
+            subtitle: "Op dit moment zijn er geen aanbiedingen beschikbaar",
+            link: "BEKIJK MEER",
+        }
+    },
+    popupauth: {
+        header: {
+            title: "",
+            subtitle: "Scan de QR-code met de Me-App",
+
         }
     }
 };
