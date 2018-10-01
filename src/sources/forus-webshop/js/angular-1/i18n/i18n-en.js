1210398d578b00636558269fa3a0286928741685
GerbenB <gerben@forus.io>
2018-10-01 11:40:59 +0200

    Added translations webshop

--- src/sources/forus-webshop/js/angular-1/i18n/i18n-en.js
+++ src/sources/forus-webshop/js/angular-1/i18n/i18n-en.js
@@ -29,6 +29,12 @@ module.exports = {
             title: "Overzicht van alle leveranciers",
             subtitle: "Binnen het forus platform werken sponsoren en leveranciers samen om de beste dienst te leveren voor het publieke domein.",
             button: 'BEKIJK DE KAART',
+        },
+        blog: {
+            title: "Onze blog",
+            name: "Blog: Nieuwe fonde Kindpakket 2019 Zuidhorn",
+            more: "Lees meer",
+            button: "BEKIJK MEER",
         }
     },
     products: {
@@ -36,6 +42,75 @@ module.exports = {
             title: "Aanbiedingen",
             select: "Selecteer categorie...",
             search: "Zoek naar een aanbieding",
+        }
+    },
+    product: {
+        header:{
+            fund: "Fonds",
+            status: "Actief",
+            findus: "Je kan ons hier vinden",
+        }
+    },
+    records: {
+        header: {
+            title: "Eigenschappen aanmaken",
+            buttons: {
+                    choose: {
+                        description: "Selecteer de categorie waaraan u de eigenschap wil toevoegen.",
+                        c_description: "Kies het type eigenschap dat u wil aanmaken.",
+                        category: "Kies categorie",
+                        type: "Kies type",
+                    },
+                cancel: "ANNULEER",
+                next: "VOLGENDE",
+                type: "EIGENSCHAP TYPE",
+                text: "TEKST",
+                back: "TERUG",
+                confirm: "BEVESTIG",
+            }
+        }
+    },
+    page_records: {
+        header: {
+            title: "Mijn eigenschappen",
+            count: "validaties",
+            validate: "Valideer",
+            create: "Eigenschap aanmaken",
+        }
+    },
+    page_voucher: {
+        header: {
+            title: "Voucher details",
+            sendtome:"E-MAIL NAAR MIJ",
+            transactions: "Transacties",
+            subtract: "Af",
+            location: "Locatie van winkel",
+            voucher: "U kunt uw voucher besteden in de winkel op deze locatie.",
+        }
+    },
+    page_vouchers: {
+        header: {
+            title: "Mijn vouchers",
+            transactions: "Transacties",
+            last: "Laatste transactie",
+            subtract: "Af",
+            none: "Geen",
+        }
+    },
+    records_validate: {
+        header: {
+            title: "Mijn eigenschappen",
+            validators: "Validators",
+            send: "VERZENDEN",
+            hold: "Wachten",
+        }
+    },
+    records_validations: {
+        header: {
+            title: "Mijn Eigenschappen",
+            count: "validaties",
+            validations: "Validaties",
+            send: "Verstuur een validatieverzoek",
         }
     },
     empty: {
@@ -47,9 +122,380 @@ module.exports = {
     },
     popupauth: {
         header: {
-            title: "",
+            title: "Inloggen op Platform Forus",
             subtitle: "Scan de QR-code met de Me-App",
+            confirmation: "Het is gelukt!",
 
         }
-    }
+    },
+    authtype: {
+        header: {
+            messageQR: "Log in via QR-Code",
+            messageEm: "Log in via E-Mail",
+        }        
+    },
+    popupfooter: {
+        header: {
+            title: "U kunt tot 14:55 uur (Nederlandse tijd) inloggen. Daarna verloopt uw sessie.",
+            subtitle: "",
+        }
+    },
+    popuptitle: {
+        header: {
+            title: "Aanmelden",
+            subtitle: "Scan deze QR-Code met een ander apparaat waar u al op aangemeld bent", 
+            subtitleEM: "Vul uw e-mail adres in om verder in te kunnen loggen",
+        }
+    },
+    form: {
+        label: {
+            message: "Vul uw e-mail adres in",
+        }
+    },
+    popup: {
+        header: {
+            title: "Aanmelden",
+            subtitle: "Er is een link naar uw e-mail adres gestuurd",
+            mail: "Vul uw e-mail adres in om een link te ontvangen waarmee u kunt inloggen",
+            mailadress: "Uw e-mail adres",
+            cancel: "Annuleer",
+        }
+    },
+    loginbycode: {
+        header: {
+            title: "Aanmelden",
+            subtitle: "Vul uw activatiecode in om aan te kunnen melden voor het kindpakket",
+            button: "VERZEND",
+            code: "Acvtivatiecode",
+            timestamp: "U kunt tot 14:55 uur (Nederlandse tijd) inloggen. Daarna verloopt uw sessie.",
+            error: "Deze code is reeds gebruikt of ongeldig",
+        }
+    },
+    placeholder: {
+        code: "Activatiecode",
+        mail: "E-mail",
+    },
+    loginbypin: {
+        header: {
+            title:"Aanmelden",
+            subtitle:"Vul uw toegangscode in om verder te gaan",
+            confirmation: "Het is gelukt!",
+        }
+    },
+    price: {
+        discount:"KORTING",
+        from:"VAN",
+        to:"TO",
+    },
+    contact: {
+        header: {
+            title: "contact",
+            subtitle: "CONTACTGEGEVENS",
+            adress:"Hooiweg 9, 9801 AJ Zuidhorn",
+            phone:"(0594) 508888",
+            mail: "kindpakket@zuidhorn.nl",
+            follow: "Volg ons",
+            name: "Naam",
+            usermail: "E-mail",
+            userphone: "Telefoon",
+            organisation: "Organisatie",
+            message: "Bericht",
+            button: "VERZENDEN",
+        }
+    },
+    emptyblock: {
+        guide: "Ontdek hoe...",
+    },
+    fund_criterion: {
+        place: "Zuidhorn",
+        validate: "Valideer",
+        invalid: "Ongeldig",
+        valid: "Geldig",
+        recordvalue: "Eigenschap waarde",
+        add: "Voeg een eigenschap toe",
+
+    },
+    maps: {
+        cancel: "Annuleren",
+    },
+    shops: {
+        header: {
+            title: "Overzicht",
+            subtitle: "Selecteer een leverancier voor meer informatie",
+            nodata: "Geen informatie",
+            mail: "E-MAIL",
+            adress: "ADRESS",
+            hours: "OPENINGSTIJDEN",
+
+        }
+    },
+    profilecard: {
+        header: {
+            heading: "Uw persoonlijke QR-code",
+            description: "Laat uw persoonlijke QR-code scannen door een validator",
+            adress: "Adres :",
+            copy: "kopieer",
+            share: "Deel",
+            item: "VOUCHER",
+            records: "EIGENSCHAPPEN",
+        }
+    },
+    funds: {
+        header: {
+            title: "Beschikbare fondsen",
+            status: "Actief",
+            categorie: "CATEGORIEËN",
+            join: "Doe mee",
+            criteria: "Voorwaarden (4)",
+            location: "Locatie",
+            office: "Zuidhorn Gemeente",
+            children: "Kinderen",
+            count: "1",
+            agelimit: "Leeftijdsgrens",
+            age: "< 18 jaar oud",
+            income: "Inkomensgrens",
+            amount: "< € 1118.46",
+            more: "BEKIJK MEER",
+        }
+    },
+    fundapply: {
+        header: {
+            title: "Mee doen",
+            criteria: "Voowaarden (4)",
+        }
+    },
+
+    //platform
+
+    csv_upload: {
+        buttons: {
+            select: "Upload .csv bestand",
+            upload: "Upload",
+            done: "Klaar",
+        },
+        text: {
+            swipe: "Sleep hier het *.CSV of *.TXT bestand",
+        },
+    },
+    fund_available: {
+        join: "Aanmelden",
+        categories: "Categorieën",
+        non: "Geen categrorieën",
+    },
+    fund_provider: {
+        hold: "Wachten",
+        accepted: "Geaccepteerd",
+        rejected: "Geweigerd",
+        stop: "Gestopt",
+        label: "Categorieën",
+        non: "Geen categrorieën",
+    },
+    fund_sponsor: {
+        buttons: {
+            join: "Aanmelden",
+            close: "Sluit",
+            pause: "Pauze",
+            restart: "Herstart",
+            settings: "Instellingen",
+            add: "Budget toevoegen",
+        },
+        status: {
+            active: "Actief",
+            paused: "Gepauzeerd",
+            closeD: "Gesloten",
+        },
+        labels: {
+            categorie: "Categorieën",
+            none: "Geen Categorieën",
+            statistics: "Statistieken",
+            transactions: "Transacties (8)",
+        }
+    },
+    modal_funds: {
+        budget: "Budget toevoegen aan fonds",
+        payment: "Maak het bedrag over naar:",
+        account: "NL83 BUNQ 3456 3344 32",
+        code: "voeg code",
+        description:"toe aan beschrijving",
+        close: "Sluiten",
+    },
+    multi_select: {
+        category: "Product categorieën",
+    },
+    photo_selector: {
+        image: "Afbeelding",
+        change: "Verander afbeelding",
+    },
+    prevalidated_table: {
+        records: "Vooringestelde eigenschappen",
+        code: "Code",
+        activated: "Geactiveerd",
+        export: "Exporteren",
+    },
+    product_card: {
+        status: {
+            active: "Actief",
+            paused: "Gepauzeerd",
+            closed: "Gesloten",
+        },
+        buttons: {
+            delete: "Verwijderen",
+            adjust: "Bewerken",
+        }
+    },
+    funds_edit: {
+        buttons: {
+            add: "Fonds toevoegen",
+            cancel: "Annuleren",
+            confirm: "Bevestigen",
+        },
+        labels: {
+            name: "Naam",
+            status: "Status",
+            start: "Startdatum",
+            end: "Einddatum",
+        }
+    },
+    funds: {
+        title: "Fondsen",
+        criteria: "Criteria's (3)",
+    },
+    office_edit: {
+        title: "Bewerk vestiging",
+        label: "Openingstijden",
+        cancel: "Annuleren",
+        confirm: "Bevestigen",
+    },
+    offices: {
+        button: {
+            asjust: "Bewerk",
+            add: "Voeg een nieuwe vestiging toe",
+            more: "Bekijk op de kaart",
+        },
+        labels: {
+            mail: "E-mail",
+            categories: "Categorieën",
+            non: "Geen categrorieën",
+            nodata: "Geen data",
+            phone: "Telefoonnummer",
+            time: "Openingstijden",
+        }
+    },
+    organization_providers: {
+        status: {
+            accepted: "Geaccepteerd",
+            rejected: "Geweigerd",
+            hold: "Wachten",
+        },
+        label: {
+            mail: "E-mail",
+            categorie: "Categorieën",
+            non: "Geen categrorieën",
+        },
+        buttons: {
+            reject: "Weigeren",
+            accept: "Accepteren",
+            join: "Aanmelding voor fonds:",
+        }
+    },
+    organization_validators: {
+        labels: {
+            address: "Adres",
+            action: "Acties",
+        },
+        buttons: {
+            adjust: "Aanpassen",
+            delete: "Verwijderen",
+            add: "Validator toevoegen",
+        }
+    },
+    organisation_edit: {
+        title: "Organisatie aanmaken",
+            labels: {
+                name: "Bedrfijsnaam",
+                account: "IBAN-nummer",
+                mail: "E-mailadres",
+                phone: "Telefoonnummer", 
+                kvk: "KvK-nummer",
+                btw: "BTW-nummer",
+            },
+            buttons: {
+                cancel: "Annuleren",
+                create: "Aanmaken",
+            }
+    },
+    organisations: {
+        buttons: {
+            choose: "Kies een organisatie om in te loggen",
+            without: "Zonder organisatie",
+            add: "Organisatie toevoegen",
+        }
+    },
+    products_edit: {
+        buttons: {
+            add: "Product toevoegen",
+            cancel: "Annuleren",
+            confirm: "Bevestig",
+        },
+        label: {
+            name: "Naam",
+            description: "Omschrijving",
+            price: "Prijs",
+            old: "Oude prijs",
+            total: "Totaal bedrag",
+            categorie: "Product categorie",
+        }
+    },
+    products: {
+        offer: "Aanbiedingen",
+        add: "Voeg een aanbieding toe",  
+    },
+    provider_indentity: {
+        label: {
+            address: "Adres",
+            mail: "E-mail",
+            offers: "Acties",
+        },
+        buttons: {
+            add: "Validator toevoegen",
+            cancel: "Annuleren",
+            confirm: "Bevestigen",
+        }
+    },
+    sign_up: {
+        labels: {
+            title: "Identiteit aanmaken",
+            mail: "E-mailadres",
+            name: "Voornaam",
+            last: "Achternaam",
+        },
+        buttons: {
+            cancel: "Annuleren",
+            next: "Volgende",
+        }
+    },
+    validations_request: {
+        buttons: {
+            accept: "Accepteren",
+            reject: "Weigeren",
+            show: "Bekijk eigenschappen",
+            validate_all: "Alles Valideren",
+            reject_all: "Alles weigeren",
+            cancel: "Annuleren",
+            confirm: "Bevestigen",
+        },
+        labels: {
+            records: "Eigenschappen",
+            bsn: "BSN",
+            requests: "Openstaande aanvragen",
+            type: "Type",
+            value: "Waarde",
+            date: "Datum",
+            results: "resultaten",
+        },
+        status: {
+            title: "Status",
+            hold: "Wachten",
+        }
+    },
 };
