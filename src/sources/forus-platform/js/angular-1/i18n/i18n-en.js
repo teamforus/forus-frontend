f01d3f7c05151a709d46ccf98ee5721f9eb872d2
GerbenB <gerben@forus.io>
2018-11-19 15:34:22 +0100

    this fixes teamforus/forus#341

--- src/sources/forus-platform/js/angular-1/i18n/i18n-en.js
+++ src/sources/forus-platform/js/angular-1/i18n/i18n-en.js
@@ -109,7 +109,7 @@ module.exports = {
     // CSV-VALIDATION = csv-validation.pug
     csv_validation: {
         header: {
-            title: "CSV uploader",
+            title: "Aanvragers toevoegen",
         },
         buttons: {
             choose: "Kies een ander fonds",
