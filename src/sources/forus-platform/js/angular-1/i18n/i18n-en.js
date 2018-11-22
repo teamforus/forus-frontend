e2a6add2e4cb080eaa322a6cf93fb922c346204e
Yarik <ateamcms@gmail.com>
2018-11-22 16:22:47 +0200

    Add warning. Can't delete product until sold out. #333

--- src/sources/forus-platform/js/angular-1/i18n/i18n-en.js
+++ src/sources/forus-platform/js/angular-1/i18n/i18n-en.js
@@ -366,6 +366,7 @@ module.exports = {
     products: {
         offers: "Aanbiedingen",
         add: "Voeg een aanbieding toe",
+        cannot_delete: 'Let op! Nadat u een aanbieding heeft toegevoegd kunt u deze niet verwijderen tot dat het totale aanbod dat u hier invult verkocht is. Ga daarom na hoe vaak u dit aanbod kunt verkopen voordat u de aanbieding toevoegt. Daarnaast vult u een verloopdatum in, dit bepaald de laatste datum dat de aanvrager uw aanbod kan ophalen.'
     },
 
     // FUNDS AVAILABLE FOR PROVIDERS = provider-funds-available.pug
