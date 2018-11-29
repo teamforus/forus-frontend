21d8b40470ec5c9f19eef67fc11b2775f295bbeb
Yarik <ateamcms@gmail.com>
2018-11-29 16:08:56 +0200

    Popup: before buying product voucher; tell non-refundable voucher #352

--- src/sources/forus-webshop/js/angular-1/components/ProductComponent.js
+++ src/sources/forus-webshop/js/angular-1/components/ProductComponent.js
@@ -1,8 +1,14 @@
+let _string = require("underscore.string");
+
 let ProductComponent = function (
     $state,
-    appConfigs
+    $filter,
+    appConfigs,
+    ModalService,
+    VoucherService
 ) {
     let $ctrl = this;
+    let vouchers = [];
 
     if (!appConfigs.features.products.show) {
         return $state.go('home');
@@ -13,11 +19,51 @@ let ProductComponent = function (
     $ctrl.$onInit = function () {
         let fundIds = $ctrl.product.funds.map(fund => fund.id);
 
-        $ctrl.isApplicable = $ctrl.vouchers.filter(function (voucher) {
+        vouchers = $ctrl.vouchers.filter(function (voucher) {
             return (fundIds.indexOf(voucher.fund_id) != -1) && (
                 $ctrl.product.price <= voucher.amount
             ) && !voucher.parent;
-        }).length > 0;
+        });
+
+        $ctrl.isApplicable = vouchers.length > 0;
+    };
+
+    $ctrl.applyProduct = () => {
+
+        if(vouchers.length == 1){
+            let voucher = vouchers[0];
+
+            let popupTitle = _string.sprintf(
+                $filter('translate')('product_apply.popup.title'),
+                $ctrl.product.name,
+                $ctrl.product.expire_at,
+                $ctrl.product.price
+            );
+
+            let popupSubDescription = _string.sprintf(
+                $filter('translate')('product_apply.popup.expiration_information'),
+                $ctrl.product.expire_at
+            );
+
+            return ModalService.open('modalNotification', {
+                type: 'confirm',
+                title: popupTitle,
+                subdescription: popupSubDescription,
+                icon: 'voucher_apply',
+                confirm: () => {
+                    return VoucherService.makeProductVoucher(
+                        voucher.address,
+                        $ctrl.product.id
+                    ).then(res => {
+                        $state.go('voucher', res.data.data);
+                    }, console.error);
+                }
+            });
+        } else {
+            return $state.go('products-apply', {
+                id: $ctrl.product.id
+            });
+        }
     };
 };
 
@@ -28,7 +74,10 @@ module.exports = {
     },
     controller: [
         '$state',
+        '$filter',
         'appConfigs',
+        'ModalService',
+        'VoucherService',
         ProductComponent
     ],
     templateUrl: 'assets/tpl/pages/product.html'
