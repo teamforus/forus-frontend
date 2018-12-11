9e31ad5ea2a86e5f328f9bc2b17eea02674ee264
maxvisser <maxvisser@gmail.com>
2018-12-11 16:36:07 +0100

    add flags to if statement home

--- src/sources/forus-platform/js/angular-1/config/flags.js
+++ src/sources/forus-platform/js/angular-1/config/flags.js
@@ -1,11 +1,26 @@
 module.exports = {
     general: {
-        maxProductCount: 2
+        maxProductCount: 2,
+        homeProducts: false,
+        homeMap: false,
+        homeSteps: false,
+        homeFaq: false,
+        homeButton: false
     },
     zuidhorn: {
-        maxProductCount: 2
+        maxProductCount: 2,
+        homeProducts: true,
+        homeMap: true,
+        homeSteps: true,
+        homeFaq: true,
+        homeButton: true
     },
     nijmegen: {
-        maxProductCount: 2
+        maxProductCount: 2,
+        homeProducts: true,
+        homeMap: true,
+        homeSteps: true,
+        homeFaq: true,
+        homeButton: true
     },
 };
