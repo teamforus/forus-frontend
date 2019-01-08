5790d365ebe9865a728f2bcdae2181053010df28
dev-rminds <dev@rminds.nl>
2019-01-08 18:14:59 +0200

    Fix export button under filters on prevalidations page

--- src/sources/forus-platform/js/angular-1/directives/PrevalidatedTableDirective.js
+++ src/sources/forus-platform/js/angular-1/directives/PrevalidatedTableDirective.js
@@ -77,9 +77,9 @@ let PrevalidatedTableDirective = async function(
         file_data,
         file_type = 'text/csv;charset=utf-8;'
     ) => {
-        var blob = new Blob([file_data]/* , {
+        var blob = new Blob([file_data], {
             type: file_type,
-        } */);
+        });
 
         FileSaver.saveAs(blob, file_name);
     };
@@ -93,13 +93,12 @@ let PrevalidatedTableDirective = async function(
     };
 
     // Export to CSV file
-    $scope.downloadAll = (filters = {}) => {
+    $scope.export = (filters = {}) => {
         PrevalidationService.export(
             JSON.parse(JSON.stringify(filters)), {
                 responseType: 'arraybuffer'
             }
         ).then((res => {
-            console.log(res);
             $scope.downloadCsv(
                 ($scope.fund.key || 'fund') + '_' + moment().format(
                     'YYYY-MM-DD HH:mm:ss'
@@ -107,8 +106,6 @@ let PrevalidatedTableDirective = async function(
                 res.data,
                 res.headers('Content-Type') + ';charset=utf-8;'
             );
-
-            $scope.init();
         }));
     };
 
