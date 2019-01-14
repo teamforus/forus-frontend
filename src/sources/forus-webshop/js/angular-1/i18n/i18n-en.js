aba86ea7bf03f8ef7a830f12fc8415fa29aad0fc
GerbenB <gerben@forus.io>
2019-01-14 11:34:32 +0100

    added insufficient amount warning message

--- src/sources/forus-webshop/js/angular-1/i18n/i18n-en.js
+++ src/sources/forus-webshop/js/angular-1/i18n/i18n-en.js
@@ -362,7 +362,8 @@ module.exports = {
         },
         buttons: {
             buy: "KOOP",
-        }
+        },
+        insufficient: "Uw tegoed is te laag. Het is hierdoor niet mogelijk om de aanbieding via de webshop te kopen. Mocht u de aanbieding alsnog willen kopen, ga dan langs bij de aanbieder. Laat uw {{fund}}-voucher zien. Uw resterend tegoed wordt afgeschreven en de rest kunt u bijbetalen." 
     },
 
     // PRODUCTS = Products.pug
