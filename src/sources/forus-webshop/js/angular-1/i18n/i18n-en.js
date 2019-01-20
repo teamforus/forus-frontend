ddd1c9e4e5753d93d7304c59259e6b237f347763
Max Visser <maxvisser@gmail.com>
2019-01-20 19:05:52 +0100

    removed .

--- src/sources/forus-webshop/js/angular-1/i18n/i18n-en.js
+++ src/sources/forus-webshop/js/angular-1/i18n/i18n-en.js
@@ -73,7 +73,7 @@ module.exports = {
                 button: 'LEES MEER OVER HOE HET SYSTEEM WERKT',
             },
             forus: {
-                title: "Forus platform en de Me-app.",
+                title: "Forus platform en de Me-app",
                 subtitle: "Forus is een jonge Groningse organisatie. Platform Forus en de Me-app zijn applicaties voor de uitgifte en afhandeling van gemeentelijke diensten en regelingen. <br /> <br /> Wilt u meer weten?",
                 button: 'LEES MEER OVER HOE HET SYSTEEM WERKT',
             },
