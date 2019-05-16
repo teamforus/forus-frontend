b8fed2719728500b4891111100cc29206a3a020f
Jamal <30194799+jamalv@users.noreply.github.com>
2019-05-16 21:01:30 +0200

    get elements from array

--- src/sources/forus-platform/js/angular-1/components/website/HallOfFameComponent.js
+++ src/sources/forus-platform/js/angular-1/components/website/HallOfFameComponent.js
@@ -3,10 +3,43 @@ let HallOfFameComponent = function(
 ) {
     let $ctrl = this;
     hofService.get().then(function(response) {
+        console.log(response.data)
+        console.log("yo")
+        $ctrl.list = response.data
 
-        $ctrl.list = response.data;
+        var list = response.data
+
+        console.log(list)
+
+        list.forEach(function(element) {
+         var image = 'https://github.com/'+element[0]+'.png?size=200'
+
+         console.log('deze shit word gecalled')
+         // console.log('hey' + image)
+
+         // $ctrl.images.append(image)
+        }
+
+        // console.log(response.data[1][0])
     });
-    hofService.get();
+    hofService.get()
+
+    $ctrl.eentest = "blabla"
+
+    console.log("jjajaaalkjlkajd")
+
+
+
+    // new variable images[]
+
+    // $ctrl.images = []
+
+    // sortedContributors.forEach(function(element) {
+    // 	var image = 'https://github.com/'+element[0]+'.png?size=200'
+
+    // 	$ctrl.images.append(image)
+    // }
+
 };
 
 module.exports = {
