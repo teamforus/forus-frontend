e205a89932ff8707253cb00bee8978debea09ae4
Max Visser <maxvisser@gmail.com>
2019-06-21 21:15:15 +0200

    moved webshop i18n files to seperate file structure

---
+++ src/sources/forus-webshop/js/angular-1/i18n/en/modals/modal-offices.js
@@ -0,0 +1,16 @@
+module.exports = {
+        
+
+    // POPUP OFFICES = popup-offices.pug
+        header: {
+            title_first_part: "We hebben ",
+            title_last_part: " aanbieders gevonden",
+            subtitle: "Selecteer een om meer informatie te zien",
+        },
+        labels: {
+            mail: "E-MAIL",
+            address: "ADRES",
+            hours: "OPENINGSTIJDEN",
+            none: "Openingstijden onbekend, raadpleeg de aanbieder.",
+        },
+}
---
+++ src/sources/forus-webshop/js/angular-1/i18n/en/pages/vouchers.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // VOUCHERs = vouchers.pug
+    header: {
+        title: "Mijn vouchers",
+    },
+    labels: {
+        transactions: "Uitgaven",
+        last: "Laatste uitgave",
+        subtract: "(Af)",
+        none: "Geen",
+        used: "GEBRUIKT",
+        generated: "AANGEMAAKT",
+        expire: "VERVALDATUM"
+    }
+}
