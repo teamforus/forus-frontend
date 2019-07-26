967633cb840c340893b6ab5cc56aca7ff26b5e74
dev-rminds <dev@rminds.nl>
2019-07-26 08:11:25 +0300

    Epic: My records page enhancement #667

--- src/sources/forus-webshop/js/angular-1/i18n/en/pages/records.js
+++ src/sources/forus-webshop/js/angular-1/i18n/en/pages/records.js
@@ -1,7 +1,7 @@
 module.exports = {
     // RECORDS = records.pug
     header: { 
-        title: "Mijn eigenschappen",
+        title: "Eigenschappen",
     },
     labels: {
         count: "validaties",
