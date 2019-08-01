0f1c4f042fedf9135e48ef26f6d4784e17b1adb9
Max Visser <maxvisser@gmail.com>
2019-08-01 22:06:19 +0200

    refactor header.scss

--- src/platforms/Website/_Website.js
+++ src/platforms/Website/_Website.js
@@ -46,7 +46,7 @@ platform.addTask('js', {
 platform.addTask('scss', {
     src: "general/website/style.scss",
     watch: [
-        "general/website/includes/**/*.scss",
+        "general/website/**/*.scss",
     ],
     path: "/",
     name: "style.min.css",
