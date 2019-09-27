e01a69081f6fcc2f997b78fce3b107644402b79a
dev-rminds <dev@rminds.nl>
2019-09-27 12:52:26 +0300

    Update front-end fund eligibility validation formula.

--- src/sources/forus-webshop/js/angular-1/services/FundService.js
+++ src/sources/forus-webshop/js/angular-1/services/FundService.js
@@ -128,55 +128,62 @@ let FundService = function(
             );
         };
 
+        let newest = (arr, prop) => {
+            let _newest = null;
+            let _newsetTime = 0;
+
+            arr.forEach(item => {
+                if (moment(item[prop]).format('X') > _newsetTime) {
+                    _newest = item;
+                }
+            });
+
+            return _newest;
+        };
+
         this.checkEligibility = (
             records = [],
             criterion,
             validators,
             organization_id = null
         ) => {
-            return records.map(function(record) {
-                let validated = record.validations.filter(function(validation) {
-                    if (organization_id && validation.organization) {
-                        if (validation.organization.id != organization_id) {
-                            return false;
-                        }
-                    }
-
-                    return (validation.state == 'approved') && validators.indexOf(
-                        validation.identity_address
-                    ) != -1;
-                }).length > 0;
-
-                let validValue = false;
-
-                if (criterion.operator == '!=') {
-                    validValue = record.value != criterion.value;
-                } else if (criterion.operator == '=') {
-                    validValue = record.value == criterion.value;
-                } else if (criterion.operator == '>') {
-                    validValue = parseFloat(record.value) > parseFloat(criterion.value);
-                } else if (criterion.operator == '<') {
-                    validValue = parseFloat(record.value) < parseFloat(criterion.value);
-                } else if (criterion.operator == '>=') {
-                    validValue = parseFloat(record.value) >= parseFloat(criterion.value);
-                } else if (criterion.operator == '<=') {
-                    validValue = parseFloat(record.value) <= parseFloat(criterion.value);
-                }
-
-                if (!validValue) {
-                    record.state = 'addRecord';
-                } else if (validated && !validValue) {
-                    record.state = 'invalid';
-                } else if (!validated && validValue) {
-                    record.state = 'validate';
-                } else if (validated && validValue) {
-                    record.state = 'valid';
-                }
-
-                return record;
+            let _records = records.filter(record => {
+                return (record.validations.filter(validation => {
+                    return (validation.organization_id == organization_id ||
+                        validation.organization_id == null) && validators.indexOf(validation.identity_address) != -1;
+                }).length > 0);
             });
-        }
 
+            if (_records.length == 0) {
+                return null;
+            }
+
+            _records.sort((a, b) => {
+                let _a = moment(newest(a.validations, 'created_at').created_at).format('X');
+                let _b = moment(newest(b.validations, 'created_at').created_at).format('X');
+
+                return _a > _b ? -1 : (_a == _b ? 0 : 1);
+            });
+
+            let record = typeof _records[0] != 'undefined' ? _records[0] : null;
+            let validValue = false;
+
+            if (criterion.operator == '!=') {
+                validValue = record.value != criterion.value;
+            } else if (criterion.operator == '=') {
+                validValue = record.value == criterion.value;
+            } else if (criterion.operator == '>') {
+                validValue = parseFloat(record.value) > parseFloat(criterion.value);
+            } else if (criterion.operator == '<') {
+                validValue = parseFloat(record.value) < parseFloat(criterion.value);
+            } else if (criterion.operator == '>=') {
+                validValue = parseFloat(record.value) >= parseFloat(criterion.value);
+            } else if (criterion.operator == '<=') {
+                validValue = parseFloat(record.value) <= parseFloat(criterion.value);
+            }
+
+            return validValue;
+        }
 
         this.fundCriteriaList = (criteria, recordsByTypesKey) => {
             return criteria.filter(
