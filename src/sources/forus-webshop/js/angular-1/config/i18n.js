1fe9ea9dcf56672d2afc02bfbab0a7381577c451
Max Visser <max@forus.io>
2019-10-28 08:01:21 +0100

    Update i18n.js

--- src/sources/forus-webshop/js/angular-1/config/i18n.js
+++ src/sources/forus-webshop/js/angular-1/config/i18n.js
@@ -1,15 +1,15 @@
 module.exports = ['$translateProvider', 'I18nLibProvider', function($translateProvider, I18nLibProvider) {
     $translateProvider.translations('nl', require('../i18n/i18n-nl'));
-    $translateProvider.translations('en', require('../i18n/i18n-en'));
+    //-$translateProvider.translations('en', require('../i18n/i18n-en'));
 
-    $translateProvider.registerAvailableLanguageKeys(['nl', 'en']);
-
-    $translateProvider.preferredLanguage('nl');
+    //-$translateProvider.registerAvailableLanguageKeys(['nl', 'en']);
+    $translateProvider.registerAvailableLanguageKeys(['nl']);
+    
+    //-$translateProvider.preferredLanguage('nl');
     $translateProvider.useSanitizeValueStrategy('escapeParameters');
     $translateProvider.useLocalStorage();
 
     I18nLibProvider.setValues({
-        nl: require('../i18n/i18n-nl-values'),
-        en: require('../i18n/i18n-en-values')
+        nl: require('../i18n/i18n-nl-values')
     });
 }];
