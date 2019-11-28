9b8c7a7de96da66845cc4e56fb33d2d2e04aa53e
aghimpu <aghimpu90@gmail.com>
2019-11-28 22:12:25 +0200

    Sponsor dashboard, add description field to funds

--- src/sources/forus-platform/js/angular-1/i18n/i18n-nl.js
+++ src/sources/forus-platform/js/angular-1/i18n/i18n-nl.js
@@ -208,6 +208,7 @@ module.exports = {
         },
         labels: {
             name: "Naam",
+            description: "Description",
             products: "Products",
             status: "Status",
             start: "Startdatum",
