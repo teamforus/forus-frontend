71a4c8202497aa84c90a70f6d6533a382d6a30d8
Max Visser <max@forus.io>
2019-12-11 08:40:04 +0100

    Update i18n-nl.js

--- src/sources/forus-platform/js/angular-1/i18n/i18n-nl.js
+++ src/sources/forus-platform/js/angular-1/i18n/i18n-nl.js
@@ -1051,8 +1051,8 @@ module.exports = {
         }
     },
     notification_preferences: {
-        title_email_preferences: 'E-mailvoorkeuren',
-        title_push_preferences: 'Push preferences',
+        title_email_preferences: 'E-mail notificaties',
+        title_push_preferences: 'Push notificaties',
         subscribe_desc: 'Met dit e-mailadres "{{email}}" bent u momenteel voor alle notificaties uitgeschreven. Wanneer u notificaties wilt ontvangen, kunt u dit hieronder per notificatie instellen.',
         unsubscribe: 'Uitschrijven voor alle notificaties',
         unsubscribe_desc:  'Ik wil me uitschrijven van alle notificaties.',
