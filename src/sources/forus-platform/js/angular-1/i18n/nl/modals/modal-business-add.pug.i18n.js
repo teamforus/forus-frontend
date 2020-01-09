326ab97f1da2627f79ff565ce4b87e6aca133f15
aghimpu <aghimpu90@gmail.com>
2020-01-09 15:46:13 +0200

    Business update modal, add description

--- src/sources/forus-platform/js/angular-1/i18n/nl/modals/modal-business-add.pug.i18n.js
+++ src/sources/forus-platform/js/angular-1/i18n/nl/modals/modal-business-add.pug.i18n.js
@@ -1,5 +1,6 @@
 module.exports = {
     title: "Kies een categorie voor organisatie {{ organization_name }}",
+    description: "Door een categorie toe te voegen bent u makkelijker te vinden.",
     labels: {
         business_type: "Organisatie type",
     },
