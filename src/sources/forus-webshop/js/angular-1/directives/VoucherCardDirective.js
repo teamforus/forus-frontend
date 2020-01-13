18aed596309e2dd715fa6e3d02124c45cdbe8de5
aghimpu <aghimpu90@gmail.com>
2020-01-13 21:36:12 +0200

    Webshop voucher page, restore cancel reservation option

--- src/sources/forus-webshop/js/angular-1/directives/VoucherCardDirective.js
+++ src/sources/forus-webshop/js/angular-1/directives/VoucherCardDirective.js
@@ -1,11 +1,33 @@
 let VoucherCardDirective = function(
     $scope,
     VoucherService,
+    ModalService
 ) {
     $scope.voucherCard = VoucherService.composeCardData($scope.voucher);
 
     $scope.voucherCard.disabled = ($scope.voucherCard.type == 'product' && $scope.voucher.transactions.length) ||
         ($scope.voucher.expired && !$scope.voucher.transactions.length);
+
+    $scope.deleteVoucher = ($event, voucher) => {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        ModalService.open('modalNotification', {
+            type: 'confirm',
+            title: 'Annuleer reservering',
+            icon: 'voucher_apply',
+            description: 'voucher.delete_voucher.popup_form.description',
+            confirmBtnText: 'voucher.delete_voucher.buttons.submit',
+            cancelBtnText: 'voucher.delete_voucher.buttons.close',
+            confirm: () => {
+                VoucherService.destroy(
+                    voucher.address
+                ).then(function(res) {
+                    $state.go('vouchers')
+                })
+            }
+        })
+    }
 };
 
 module.exports = () => {
@@ -18,6 +40,7 @@ module.exports = () => {
         controller: [
             '$scope',
             'VoucherService',
+            'ModalService',
             VoucherCardDirective
         ],
         templateUrl: 'assets/tpl/directives/voucher-card.html' 
