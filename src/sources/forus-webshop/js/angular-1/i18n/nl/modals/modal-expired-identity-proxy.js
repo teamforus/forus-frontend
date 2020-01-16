1ca68a7c57da312899d7e1c1abedfeb45e9efc10
Max Visser <max@forus.io>
2020-01-16 07:40:21 +0100

    Update modal-expired-identity-proxy.js

--- src/sources/forus-webshop/js/angular-1/i18n/nl/modals/modal-expired-identity-proxy.js
+++ src/sources/forus-webshop/js/angular-1/i18n/nl/modals/modal-expired-identity-proxy.js
@@ -1,4 +1,4 @@
 module.exports = {
-    title: "Gebruikt of niet meer geldig link",
-    description: "Helaas, het is niet gelukt om in te loggen. De link is reeds gebruikt of niet meer geldig. Probeer het opnieuw met een andere link.",
+    title: "De inloglink is al gebruikt of niet meer geldig.",
+    description: "Helaas, het is niet gelukt om in te loggen. De link is al gebruikt of niet meer geldig. Vraag een nieuwe inloglink aan.",
 }
