f026634fc5750f202b289b8f5999ef8e9535633b
Sjoerd Hilhorst <hilhorstsjoerd@gmail.com>
2020-03-18 16:24:50 +0100

    updated protractor-env.sample

--- src/test/protractor-env.sample.js
+++ src/test/protractor-env.sample.js
@@ -13,13 +13,12 @@ var environment = {
     password : "your-email-client-password",
     
     //test environment
-    browserName : "firefox",
-    customerURL : "http://localhost:5560",
-    providerURL : "http://localhost:4060",
-    //providerURL : "https://staging.forus.io/provider",
-   
+    browserName : "chrome",
+    customerURL : "http://localhost:5500",
+    providerURL : "http://localhost:4000",
+    sponsorURL :  "http://localhost:3500",
+    active_account : "your session token",
 
-    
     //input variables
     emailSibling : "your-email-client+".concat(r, "@gmail.com"),
     activationcode : "94e0-b1f6",
