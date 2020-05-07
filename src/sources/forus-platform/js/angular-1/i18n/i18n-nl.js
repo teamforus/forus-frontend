e20de4920484065b9a3c4454b3e988d285193297
aghimpu <aghimpu90@gmail.com>
2020-05-07 21:29:33 +0300

    Financial dashboard, add year filtration for provider statistics

--- src/sources/forus-platform/js/angular-1/i18n/i18n-nl.js
+++ src/sources/forus-platform/js/angular-1/i18n/i18n-nl.js
@@ -1326,6 +1326,7 @@ module.exports = {
             categories: "Categorieën",
             nocategories: "Geen categorieën",
             join: "Aanmelding voor fonds",
+            year: "Jaar",
             quarter: "Kwartaal",
             month: "Maand",
             week: "Week",
