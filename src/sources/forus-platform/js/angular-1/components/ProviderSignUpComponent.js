3874b6b12c2722a20b3b03458f5b9508c1577237
aghimpu <aghimpu90@gmail.com>
2020-07-24 02:54:46 +0300

    Provider sign up, mobile skip step 2 adjustments

--- src/sources/forus-platform/js/angular-1/components/ProviderSignUpComponent.js
+++ src/sources/forus-platform/js/angular-1/components/ProviderSignUpComponent.js
@@ -453,6 +453,11 @@ let ProviderSignUpComponent = function(
     };
 
     $ctrl.setStep = (step) => {
+        if (isMobile() && step == $ctrl.STEP_INFO_ME_APP) {
+            $ctrl.step = step;
+            return step >= $ctrl.step ? $ctrl.next() : $ctrl.back();
+        }
+
         if (step <= $ctrl.STEP_SIGNUP_FINISHED) {
             $ctrl.step = step;
             progressStorage.set('step', step);
@@ -555,11 +560,6 @@ let ProviderSignUpComponent = function(
     };
 
     $ctrl.next = function() {
-        if (isMobile() && $ctrl.step == $ctrl.STEP_INFO_GENERAL) {
-            $ctrl.setStep($ctrl.STEP_SCAN_QR);
-            return;
-        }
-        
         if ($ctrl.step == $ctrl.STEP_ORGANIZATION_ADD) {
             let submit = () => $ctrl.organizationForm.submit().then((res) => {
                 $ctrl.setOrganization(res.data.data);
