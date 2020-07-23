a8c1d6f6912c1a8d7dfaae39d2d6c441fc09dcb3
GerbenB <gerben@forus.io>
2020-07-23 22:22:33 +0200

    groningen update

--- src/sources/forus-webshop/js/angular-1/i18n/nl/components/buttons.js
+++ src/sources/forus-webshop/js/angular-1/i18n/nl/components/buttons.js
@@ -20,6 +20,6 @@ module.exports = {
     create: "Eigenschap aanmaken",
     send: "E-Mail naar mij",
     details: "Bekjk details",
-    show_map: "BEKIJK",
+    show_map: "BEKIJK DE KAART",
     start: 'start', 
 }
