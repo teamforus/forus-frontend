8c13d02149ededbc9f9ddab842a092676ca9775e
dev-rminds <dev@rminds.nl>
2020-07-30 11:58:22 +0300

    fix business type modal on sign-up flow

--- src/sources/forus-platform/js/angular-1/routers/router.js
+++ src/sources/forus-platform/js/angular-1/routers/router.js
@@ -20,15 +20,18 @@ let objectOnlyKeys = (obj, keys) => {
     return out;
 };
 
-let handleAuthTarget = ($state, target, appConfigs) => {
+let handleAuthTarget = ($state, target) => {
     if (target[0] == targetHome) {
-        return !!$state.go('home', {
+        $state.go('home', {
             confirmed: true
         });
+
+        return true;
     }
 
     if (target[0] == targetNewSignup) {
-        return !!$state.go('sign-up');
+        $state.go('sign-up');
+        return true;
     }
 
     return false;
@@ -983,17 +986,19 @@ module.exports = ['$stateProvider', '$locationProvider', 'appConfigs', (
                         PermissionsService.hasPermission(organization, 'manage_organization')
                     );
 
-                    if (appConfigs.panel_type != 'provider' || organizations.length == 0) {
-                        if (typeof target != 'string' || !handleAuthTarget($state, target.split('-'), appConfigs)) {
+                    let onReady = () => {
+                        if (typeof target != 'string' || !handleAuthTarget($state, target.split('-'))) {
                             return $state.go('organizations');
                         }
-                    }
+                    };
 
-                    if (target.split('-') != targetNewSignup) {
+                    if (organizations.length > 0) {
                         ModalService.open('businessSelect', {
                             organizations: organizations,
-                            onReady: () => $state.go('organizations')
+                            onReady: () => onReady(),
                         });
+                    } else {
+                        onReady();
                     }
                 });
             }, () => {
@@ -1024,7 +1029,7 @@ module.exports = ['$stateProvider', '$locationProvider', 'appConfigs', (
             ).then(function(res) {
                 CredentialsService.set(res.data.access_token);
                 $rootScope.loadAuthUser().then(() => {
-                    if (typeof target != 'string' || !handleAuthTarget($state, target.split('-'), appConfigs)) {
+                    if (typeof target != 'string' || !handleAuthTarget($state, target.split('-'))) {
                         $state.go('home', {
                             confirmed: 1
                         });
