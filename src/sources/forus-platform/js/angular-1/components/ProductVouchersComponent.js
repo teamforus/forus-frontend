d0e5acbca5c8a92b3c4f737a6b2635a0803878f4
aghimpu <aghimpu90@gmail.com>
2020-08-10 23:16:33 +0300

    Product voucher generator creation, fix typo

--- src/sources/forus-platform/js/angular-1/components/ProductVouchersComponent.js
+++ src/sources/forus-platform/js/angular-1/components/ProductVouchersComponent.js
@@ -89,7 +89,7 @@ let ProductVouchersComponent = function(
 
         VoucherService.index(
             $ctrl.organization.id,
-            Object.assign(query, {
+            Object.assign(_query, {
                 fund_id: $ctrl.fund.id,
             })
         ).then((res => {
