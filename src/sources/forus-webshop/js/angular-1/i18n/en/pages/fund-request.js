da504bad05497abca11618cb1745bcfcf783fc14
GerbenB <gerben@forus.io>
2020-09-03 12:14:09 +0200

    trextual updates added breadcrumbs

--- src/sources/forus-webshop/js/angular-1/i18n/en/pages/fund-request.js
+++ src/sources/forus-webshop/js/angular-1/i18n/en/pages/fund-request.js
@@ -4,7 +4,7 @@ module.exports = {
         block_title: "{{ fundname }} aanvragen",
         pane: {
             header_title: "Overzicht",
-            text: "U staat op het punt om een {{ fundname }} aan te vragen. Om in aanmmerking te komen, dient u aan de voorwaarden te voldoen:",
+            text: "U staat op het punt om een {{ fundname }} aan te vragen. Om in aanmerking te komen, dient u aan de voorwaarden te voldoen:",
             criterion_more: "'{{ name }}' moet meer dan {{ value }} zijn.",
             criterion_less: "'{{ name }}' moet minder dan {{ value }} zijn.",
             criterion_same: "'{{ name }}' moet {{ value }} zijn.",
