6cd5547a54a6be4ab4f0a3f3c86f4b0101484ac0
Max Visser <maxvisser@gmail.com>
2020-11-15 19:31:26 +0100

    Added a privacy page

--- src/sources/forus-webshop/js/angular-1/angular-app.js
+++ src/sources/forus-webshop/js/angular-1/angular-app.js
@@ -37,6 +37,7 @@ app.component('recordCreateComponent', require('./components/RecordCreateCompone
 app.component('meComponent', require('./components/MeComponent'));
 app.component('emailPreferencesComponent', require('./components/EmailPreferencesComponent'));
 app.component('accessibilityComponent', require('./components/AccessibilityComponent'));
+app.component('privacyComponent', require('./components/PrivacyComponent'));
 app.component('errorComponent', require('./components/ErrorComponent'));
 app.component('securitySessionsComponent', require('./components/SecuritySessionsComponent'));
 app.component('identityEmailsComponent', require('./components/IdentityEmailsComponent'));
