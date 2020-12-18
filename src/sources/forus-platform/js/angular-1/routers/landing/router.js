325ac7f828122da0c735ecf284c7e3d562a1b7eb
dev-rminds <dev@rminds.nl>
2020-12-18 10:27:41 +0200

    website download app url rename dl to DL

--- src/sources/forus-platform/js/angular-1/routers/landing/router.js
+++ src/sources/forus-platform/js/angular-1/routers/landing/router.js
@@ -59,7 +59,7 @@ module.exports = ['$stateProvider', '$locationProvider', 'appConfigs', function(
         name: "hall-of-fame",
         url: "/hall-of-fame",
         component: "hallOfFameComponent"
-    });  
+    });
 
     $stateProvider.state({
         name: "me",
@@ -93,8 +93,14 @@ module.exports = ['$stateProvider', '$locationProvider', 'appConfigs', function(
 
     $stateProvider.state({
         name: "dl",
-        url: "/dl",
+        url: "/DL",
         component: "dlMeAppComponent"
+    });
+
+    $stateProvider.state({
+        name: "dl_lc",
+        url: "/dl",
+        controller: ['$state', ($state) => $state.go('dl')]
     });
 
     $stateProvider.state({
