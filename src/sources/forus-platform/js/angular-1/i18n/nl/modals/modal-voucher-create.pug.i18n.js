e70bf682135ca829ecd8df4fce6572d25e5806df
Max Visser <max@forus.io>
2020-12-18 10:43:51 +0100

    Update modal-voucher-create.pug.i18n.js

--- src/sources/forus-platform/js/angular-1/i18n/nl/modals/modal-voucher-create.pug.i18n.js
+++ src/sources/forus-platform/js/angular-1/i18n/nl/modals/modal-voucher-create.pug.i18n.js
@@ -12,7 +12,7 @@ module.exports = {
         choose_title: "Selecteer op welke manier u een tegoed wilt aanmaken.",
         choose_subtitle: "Als u een activatiecode bij de hand hebt, gebruik dan de tweede optie.",
         activation_code_title: "Vul een activatiecode in.",
-        activation_code_subtitle: "Het systeem zal controleren of de activatie code nog niet gebruikt is, \n wanneer hij niet gebruikt is zal hij worden gebruikt om een tegoed te genereren.",
+        activation_code_subtitle: "Het systeem zal controleren of de activatiecode nog niet gebruikt is, \n wanneer hij niet gebruikt is zal hij worden gebruikt om een tegoed te genereren.",
         voucher_type_item: {
             giftcard: "Normaal",
             activation_code: "via activatie-code"
@@ -27,7 +27,7 @@ module.exports = {
         need_providers: "Mislukt, dit fonds heeft geen goedgekeurde aanbieders met aanbiedingen.",
         no_products: "U moet eerst aanbieders goedkeuren die aanbiedingen hebben geplaatst.",
     },
-    info: "Tegoed eenmaal aangemaakt kan niet verwijderd worden. Met een tegoed gemaakt via deze functie kan een gebruiker gebruik maken van een budget zonder account zolang niet toegekend.",
+    info: "Een tegoed eenmaal aangemaakt kan niet verwijderd worden.",
     buttons: {
         cancel: "Annuleren",
         submit: "Bevestig",
