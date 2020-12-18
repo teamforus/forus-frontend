2ac99d635c768496b1955ade0c03aace3332c2e5
dev-rminds <dev@rminds.nl>
2020-12-18 09:02:26 +0200

    website /dl page refactoring

--- src/sources/forus-platform/js/angular-1/components/landing/DlMeAppComponent.js
+++ src/sources/forus-platform/js/angular-1/components/landing/DlMeAppComponent.js
@@ -1,13 +1,6 @@
-let DlMeAppComponent = function(
-    $state,
-    $rootScope,
-    $timeout
-) {
+let DlMeAppComponent = function(appConfigs) {
     let $ctrl = this;
-    $ctrl.iosLink = "https://testflight.apple.com/join/gWw1lXyB";
-    $ctrl.androidLink = "https://media.forus.io/static/me-0.0.5-staging-7-release.apk";
-
-    let iDevices = [
+    let iosPlatforms = [
         'iPad Simulator',
         'iPhone Simulator',
         'iPod Simulator',
@@ -16,39 +9,24 @@ let DlMeAppComponent = function(
         'iPod'
     ];
 
-    this.isiOS = () => {
-        if (!!navigator.platform) {
-            while (iDevices.length) {
-                if (navigator.platform === iDevices.pop()) { 
-                    return true; 
-                }
-            }
-        }
-    
-        return false;
-    }
-    
-    this.isAndroid = () => {
-        return navigator.userAgent.toLowerCase().indexOf("android") > -1;
-    }
-
     $ctrl.$onInit = () => {
-        if (this.isiOS()){
-            $timeout(function() {
-                document.location.href = $ctrl.iosLink; 
-            }, 0);
-        } else if (this.isAndroid()) {
-            $timeout(function() {
-                document.location.href = $ctrl.androidLink; 
-            }, 0);
+        $ctrl.iosLink = appConfigs.ios_link;
+        $ctrl.androidLink = appConfigs.android_link;
+
+        $ctrl.isiOS = navigator.platform && iosPlatforms.indexOf(navigator.platform) !== -1;
+        $ctrl.isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1;
+
+        if ($ctrl.isiOS) {
+            document.location.href = $ctrl.iosLink;
+        } else if ($ctrl.isAndroid) {
+            document.location.href = $ctrl.androidLink;
         }
     };
 };
 
 module.exports = {
     controller: [
-        '$state',
-        '$rootScope',
+        'appConfigs',
         '$timeout',
         DlMeAppComponent
     ],
