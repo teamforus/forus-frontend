3bfdcb39c2c3222537f99dd73f341b40c8ef814e
Sjoerd Hilhorst <hilhorstsjoerd@gmail.com>
2021-01-02 11:03:58 +0100

    update me app text

--- src/sources/forus-platform/js/angular-1/i18n/i18n-nl.js
+++ src/sources/forus-platform/js/angular-1/i18n/i18n-nl.js
@@ -955,11 +955,10 @@ module.exports = {
         app: {
             title: "Heeft u de Me-app al?",
             description_top: [
-                "De Me-app is een optionele manier om eenvoudig, veilig en snel op deze website in te loggen, en om uw budgetten te beheren.",
+                "Scan de QR-code aan de rechterzijde met de QR-scanner in de Me-app.",
             ].join("\n"),
             description_bottom: [
-                "Scan de QR-code aan de rechterzijde met de QR-scanner in de Me-app.",
-                "De Me-app wordt gebruikt om makkelijk en veilig in te loggen, betalingen te doen en vouchers te beheren"
+                "De Me-app wordt gebruikt om makkelijk en veilig in te loggen, betalingen te doen en tegoeden te beheren"
             ].join("\n"),
             no_app: "Ik wil inloggen met mijn e-mailadres >"
         },
@@ -1083,11 +1082,9 @@ module.exports = {
         app: {
             title: "Heeft u de Me-app al?",
             description_top: [
-                "De Me-app is een optionele manier om eenvoudig, veilig en snel op deze website in te loggen, en om uw budgetten te beheren.",
                 "Scan de QR-code aan de rechterzijde met de QR-scanner in de Me-app.",
             ].join("\n"),
             description_bottom: [
-                "Scan de QR-code aan de rechterzijde met de QR-scanner in de Me-app.", 
                 "De Me-app wordt gebruikt om makkelijk en veilig in te loggen, betalingen te doen en vouchers te beheren"
             ].join("\n"),
             no_app: "Ik wil inloggen met mijn e-mailadres >"    
