4efcbc1609d925a3084c18c99a3900a89ba92e71
aghimpu <aghimpu90@gmail.com>
2021-01-07 23:00:37 +0200

    Webshop, provider page new text below the button

--- src/sources/forus-webshop/js/angular-1/components/SignUpSelectionComponent.js
+++ src/sources/forus-webshop/js/angular-1/components/SignUpSelectionComponent.js
@@ -1,9 +1,17 @@
 let SignUpSelectionComponent = function(appConfigs) {
     let $ctrl = this;
 
+    $ctrl.getDashboardAddress = (clientType) => {
+        return appConfigs.features.fronts['url_' + clientType];
+    };
+
+    $ctrl.goToDashboard = (clientType) => {
+        document.location.href = $ctrl.getDashboardAddress(clientType);
+    };
+
     $ctrl.goToSignUpPage = (clientType) => {
-        document.location.href = appConfigs.features.fronts['url_' + clientType] + 'sign-up';
-    }
+        document.location.href = $ctrl.getDashboardAddress(clientType) + 'sign-up';
+    };
 };
 
 module.exports = {
