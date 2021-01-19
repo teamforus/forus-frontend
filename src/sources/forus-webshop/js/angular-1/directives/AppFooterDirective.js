1a4a7b9660f70d19f79c210e552469c9fae3b810
Max Visser <maxvisser@gmail.com>
2021-01-19 13:15:50 +0100

    Refactored footer, Implmented footer fields

--- src/sources/forus-webshop/js/angular-1/directives/AppFooterDirective.js
+++ src/sources/forus-webshop/js/angular-1/directives/AppFooterDirective.js
@@ -1,11 +1,30 @@
-let AppFooterDirective = function() {};
+let AppFooterDirective = function(
+    $sce,
+    $scope,
+    appConfigs
+) { 
+    $scope.appConfigs = appConfigs;
+    $scope.$watch('appConfigs', (_appConfigs) => {
+        if (_appConfigs.features && _appConfigs.features.settings) {
+            $scope.settings = $scope.appConfigs.features.settings;
+            $scope.description_contact_details_html = $sce.trustAsHtml($scope.appConfigs.features.settings.description_contact_details_html);
+            $scope.description_opening_times_html = $sce.trustAsHtml($scope.appConfigs.features.settings.description_opening_times_html);
+        }
+    }, true);
+};
+
 
 module.exports = () => {
     return {
-        scope: {},
+        scope: {
+            settings: '=?'
+        },
         restrict: "EA",
         replace: true,
         controller: [
+            '$sce',
+            '$scope',
+            'appConfigs',
             AppFooterDirective
         ],
         templateUrl: 'assets/tpl/directives/app-footer.html' 
