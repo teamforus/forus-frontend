92485d4a9835c035303d9cf1bfa43a086583f364
aghimpu <aghimpu90@gmail.com>
2021-01-23 19:45:04 +0200

    Provider signup flow, hide dashboard switcher

--- src/sources/forus-platform/js/angular-1/directives/DashboardSwitcherDirective.js
+++ src/sources/forus-platform/js/angular-1/directives/DashboardSwitcherDirective.js
@@ -1,4 +1,10 @@
-let DashboardSwitcherDirective = function(){};
+let DashboardSwitcherDirective = function($scope){
+    $scope.hideDashboardSwitcher = false;
+    
+    $scope.$watchCollection(() => [$scope.$root.viewLayout, $scope.$root.auth_user], ([viewLayout, auth_user]) => {
+        $scope.hideDashboardSwitcher = !auth_user || viewLayout == 'signup';
+    });
+};
 
 module.exports = () => {
     return {
@@ -6,7 +12,7 @@ module.exports = () => {
         replace: true,
 
         controller: [
-            'appConfigs',
+            '$scope',
             DashboardSwitcherDirective
         ],
         templateUrl: 'assets/tpl/directives/dashboard-switcher.html' 
