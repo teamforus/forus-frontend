a66ca71d9b2675125a80670bb9b4e59f3675dd37
Max Visser <max@forus.io>
2021-05-27 09:29:07 +0200

    remove unused i18n texts

--- src/sources/forus-platform/js/angular-1/i18n/nl/modals/modal-transfer-organization-ownership.pug.i18n.js
+++ src/sources/forus-platform/js/angular-1/i18n/nl/modals/modal-transfer-organization-ownership.pug.i18n.js
@@ -8,14 +8,4 @@ module.exports = {
         cancel: "Annuleer",
         submit: "Bevestig",
     },
-    modal_section: {
-        choose_title: "Selecteer op welke manier u een QR-code voor een aanbod wilt aanmaken.",
-        choose_subtitle: "Als u een activatiecode bij de hand hebt, gebruik dan de tweede optie.",
-        activation_code_title: "Vul een activatiecode in.",
-        activation_code_subtitle: "Het systeem zal controleren of de activatie code nog niet gebruikt is, \n wanneer hij niet gebruikt is zal hij worden gebruikt om een QR-code te genereren.",
-        voucher_type_item: {
-            giftcard: "Normaal",
-            activation_code: "via activatie-code"
-        }
-    },
 };
