9bdeed8911f1c9f82f6f892d2dd893c2ab3d4474
Max Visser <max@forus.io>
2021-06-21 09:20:05 +0200

    Update remove-provider-application.js

--- src/sources/forus-platform/js/angular-1/i18n/nl/modals/danger-zone/remove-provider-application.js
+++ src/sources/forus-platform/js/angular-1/i18n/nl/modals/danger-zone/remove-provider-application.js
@@ -1,9 +1,9 @@
 module.exports = {
-    title: "Let op! De aanmelding wordt geanuleerd",
-    description: `Na annuleren zal {{sponsor_organisation_name}} de aanmelding niet meer behandelen. Het is mogelijk om je opnieuw aan te melden.\n\n` +
-        'Weet je zeker dat je wilt annuleren?',
+    title: "Let op! De aanmelding wordt geannuleerd",
+    description: `Na annuleren zal {{sponsor_organisation_name}} de aanmelding niet meer behandelen. Het is mogelijk om u zelf opnieuw aan te melden.\n\n` +
+        'Weet u zeker dat u wilt annuleren?',
     buttons: {
-        cancel: 'Cancel',
-        confirm: 'Annuleren',
+        cancel: 'Annuleren',
+        confirm: 'Bevestigen',
     },
 };
