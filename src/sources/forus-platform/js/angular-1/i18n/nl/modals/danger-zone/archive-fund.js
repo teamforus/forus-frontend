7a3ef50e446e2769712a7385dc8e0cbd0230bbca
Max Visser <max@forus.io>
2021-08-06 17:21:23 +0200

    Translated archive funds modal

--- src/sources/forus-platform/js/angular-1/i18n/nl/modals/danger-zone/archive-fund.js
+++ src/sources/forus-platform/js/angular-1/i18n/nl/modals/danger-zone/archive-fund.js
@@ -1,6 +1,6 @@
 module.exports = {
-    title: "Fund archive!",
-    description: `This will hide the fund from any statistics and you won't be able to perform any action on it.`,
+    title: "Weet u zeker dat u dit fonds wilt archiveren?",
+    description: `U staat op het punt om een fonds te archiveren. Het fonds zal alleen zichtbaar zijn op de archief pagina, waar u dit fonds ook weer uit het archief kunt halen.`,
     buttons: {
         cancel: 'Annuleren',
         confirm: 'Bevestigen',
