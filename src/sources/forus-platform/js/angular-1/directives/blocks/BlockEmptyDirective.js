b278fb86747aee76118a83fe515a65ddf31e8bd8
dev-rminds <dev@rminds.nl>
2021-10-02 02:03:25 +0300

    fix empty block 'align' param

--- src/sources/forus-platform/js/angular-1/directives/blocks/BlockEmptyDirective.js
+++ src/sources/forus-platform/js/angular-1/directives/blocks/BlockEmptyDirective.js
@@ -8,7 +8,7 @@ module.exports = () => {
             title: '@',
             text: '@',
             button: '=',
-            align: '=',
+            align: '@',
         },
         restrict: "EA",
         replace: true,
