b34f1d3d46ab3224a916ce0f3d36507b9bc3bca2
Max Visser <maxvisser@gmail.com>
2021-12-02 16:42:42 +0100

    WCAG fixes v3

--- src/sources/forus-platform/js/angular-1/directives/MarkdownDirective.js
+++ src/sources/forus-platform/js/angular-1/directives/MarkdownDirective.js
@@ -215,7 +215,7 @@ const MarkdownDirective = function($scope, $element, $timeout, ModalService) {
             ], ...(allowLists ? [
                 ['para', ['ol', 'ul']],
             ] : []), ...(extendedOptions ? [
-                ['table', ['table']],
+                /* ['table', ['table']], */
                 ['cms', ['cmsLink', 'unlink', 'cmsMedia', 'cmsLinkYoutube']],
             ] : [
                 ['cms', ['cmsLink', 'unlink']]
@@ -248,7 +248,7 @@ const MarkdownDirective = function($scope, $element, $timeout, ModalService) {
                         }
                     });
 
-                    if (turndownPluginGfm) {
+                    if (typeof turndownPluginGfm != 'undefined') {
                         turndownService.use(turndownPluginGfm.gfm);
                     }
 
