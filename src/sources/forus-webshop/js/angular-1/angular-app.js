22b6468d9ef8b8c528d961a75ddfe87ade597501
Ghimpu Alexandru <aghimpu90@gmail.com>
2021-12-03 16:41:37 +0200

    Provider signup page on webshop, adjust font sizes (#1967)
    Co-authored-by: dev-rminds <dev@rminds.nl>

--- src/sources/forus-webshop/js/angular-1/angular-app.js
+++ src/sources/forus-webshop/js/angular-1/angular-app.js
@@ -20,7 +20,7 @@ app.controller('BaseController', require('./controllers/BaseController'));
 // Components
 app.component('homeComponent', require('./components/HomeComponent'));
 app.component('signUpComponent', require('./components/SignUpComponent')); // todo: cleanup
-app.component('signUpSelectionComponent', require('./components/SignUpSelectionComponent'));
+app.component('signUpProviderComponent', require('./components/SignUpProviderComponent'));
 app.component('fundsComponent', require('./components/FundsComponent'));
 app.component('vouchersComponent', require('./components/VouchersComponent'));
 app.component('reservationsComponent', require('./components/ReservationsComponent'));
