8b37a36633f753c7c1fd14011f3174506ea14111
Yaroslav <ateamcms@gmail.com>
2022-04-08 16:19:50 +0300

    move iconnect credentials to fund config

--- src/sources/forus-platform/js/angular-1/services/PersonBSNService.js
+++ src/sources/forus-platform/js/angular-1/services/PersonBSNService.js
@@ -1,7 +1,9 @@
 const PersonBSNService = function(ApiRequest) {
     return new(function() {
-        this.read = (organization_id, bsn) => {
-            return ApiRequest.get(`/platform/organizations/${organization_id}/person-bsn/${bsn}`);
+        this.read = (organization_id, fund_id, bsn) => {
+            return ApiRequest.get(
+                `/platform/organizations/${organization_id}/funds/${fund_id}/person-bsn/${bsn}`
+            );
         };
     });
 };
