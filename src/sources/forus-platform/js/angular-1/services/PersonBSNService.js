74377077973da1afd66503396405db795a86ae00
Yaroslav <ateamcms@gmail.com>
2022-04-11 18:51:34 +0300

    fix url for person bsn service

--- src/sources/forus-platform/js/angular-1/services/PersonBSNService.js
+++ src/sources/forus-platform/js/angular-1/services/PersonBSNService.js
@@ -1,9 +1,9 @@
 const PersonBSNService = function(ApiRequest) {
     return new(function() {
         this.read = (organization_id, fund_id, bsn) => {
-            return ApiRequest.get(
-                `/platform/organizations/${organization_id}/funds/${fund_id}/person-bsn/${bsn}`
-            );
+            return ApiRequest.post(`/platform/organizations/${organization_id}/funds/${fund_id}/person-bsn`, {
+                'bsn': bsn
+            });
         };
     });
 };
