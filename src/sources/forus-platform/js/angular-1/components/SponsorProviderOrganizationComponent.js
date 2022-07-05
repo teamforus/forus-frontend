b73ca1cf4096a2812fe906ccc9440592580c05e3
dev-rminds <dev@rminds.nl>
2022-07-06 02:12:54 +0300

    refactoring provider state update confirmation modal

--- src/sources/forus-platform/js/angular-1/components/SponsorProviderOrganizationComponent.js
+++ src/sources/forus-platform/js/angular-1/components/SponsorProviderOrganizationComponent.js
@@ -1,20 +1,13 @@
-const SponsorProviderOrganizationComponent = function(
+const SponsorProviderOrganizationComponent = function (
     $q,
-    $filter,
     FundService,
-    ModalService,
     OrganizationService,
     PushNotificationsService
 ) {
     const $ctrl = this;
-    const $translate = $filter('translate');
 
     const filters = { values: { q: "", per_page: 2 } };
     const providerFilters = { values: { q: "", per_page: 10 } };
-
-    const $translateDangerZone = (key) => $translate(
-        'modals.danger_zone.sponsor_provider_organization_state.' + key
-    );
 
     const fetchFundProviders = (providerOrganization, query = {}) => {
         return $q((resolve, reject) => OrganizationService.listProviders(
@@ -33,71 +26,16 @@ const SponsorProviderOrganizationComponent = function(
         return { ...fundProvider, srefParams };
     };
 
-    const rejectFundProvider = (fundProvider) => {
-        const state = 'rejected';
-
-        ModalService.open("dangerZone", {
-            title: $translateDangerZone(state + '.title'),
-            description: $translateDangerZone(state + '.description'),
-            cancelButton: $translateDangerZone(state + '.buttons.cancel'),
-            confirmButton: $translateDangerZone(state + '.buttons.confirm'),
-            text_align: 'center',
-            onConfirm: () => {
-                fundProvider.submittingState = state;
-        
-                $ctrl.updateProvider(fundProvider, { state }).finally(() => {
-                    fundProvider.submittingState = false;
-                });
-            }
-        });
-    };
-
-    const acceptFundProvider = (fundProvider) => {
-        const state = 'accepted';
-        const fields = [{
-            key: 'allow_budget',
-            name: 'Allow budget'
-        }, {
-            key: 'allow_products',
-            name: 'Allow products'
-        }];
-
-        const makeSections = (fields) => ([
-            { type: "checkbox", key: "fields", fields, fieldsPerRow: 2, selectAll: true, title: $translateDangerZone(state + '.options') },
-        ]);
-
-        const onSuccess = (data) => {
-            const { fields } = data;
-            const queryFilters = { 
-                state: state, 
-                allow_budget: fields.indexOf('allow_budget') != -1, 
-                allow_products: fields.indexOf('allow_products') != -1
-            };
-
-            fundProvider.submittingState = state;
-    
-            $ctrl.updateProvider(fundProvider, queryFilters).finally(() => {
-                fundProvider.submittingState = false;
-            });
-        };
-
-        ModalService.open('exportDataSelect', {
-            title: $translateDangerZone(state + '.title'),
-            description: $translateDangerZone(state + '.description'),
-            fields: fields,
-            sections: makeSections(fields),
-            success: onSuccess
-        });
-    };
-
     $ctrl.updateFundProviderState = (fundProvider, accepted) => {
         const state = accepted ? 'accepted' : 'rejected';
 
-        if (state == 'rejected') {
-            rejectFundProvider(fundProvider);
-        } else {
-            acceptFundProvider(fundProvider);
-        }
+        FundService.confirmFundProviderStateUpdate(fundProvider, state).then((data) => {
+            fundProvider.submittingState = state;
+
+            $ctrl.updateProvider(fundProvider, data).finally(() => {
+                fundProvider.submittingState = false;
+            });
+        });
     };
 
     $ctrl.updateProvider = (fundProvider, query) => {
@@ -134,9 +72,7 @@ module.exports = {
     },
     controller: [
         '$q',
-        '$filter',
         'FundService',
-        'ModalService',
         'OrganizationService',
         'PushNotificationsService',
         SponsorProviderOrganizationComponent
