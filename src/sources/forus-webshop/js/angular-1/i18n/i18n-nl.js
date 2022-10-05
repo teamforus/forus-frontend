164406a864b198234d277eb92ecda25598c8f385
Gerben <gerben@forus.io>
2022-10-05 11:05:04 +0200

    update ppm

--- src/sources/forus-webshop/js/angular-1/i18n/i18n-nl.js
+++ src/sources/forus-webshop/js/angular-1/i18n/i18n-nl.js
@@ -63,6 +63,7 @@ module.exports = {
         schagen: "Schagen",
         hartvanwestbrabant: "HvWB",
         participatiemunt: "Participatiemunt",
+        eemsdelta: "Eemsdelta",
     },
     languages: {
         en: 'English',
