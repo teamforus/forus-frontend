bb976b5d13b3c501ac345b7ff2f95007686d3a1d
Gerben <gerben@forus.io>
2022-12-19 01:15:54 +0100

    update ede

--- src/sources/forus-webshop/js/angular-1/i18n/nl/pages/signup.js
+++ src/sources/forus-webshop/js/angular-1/i18n/nl/pages/signup.js
@@ -5,12 +5,16 @@ module.exports = {
         header: "Hoe wilt u aanmelden?",
         pane_text: "Account herstellen of met een andere optie inloggen?",
         digid: "wordt doorverwezen naar DigiD om aan te melden",
+        restore_title: "Account herstellen",
+        restore_header: "",
         ede: {
             title: "Inloggen",
             header: "Hier logt u in",
             pane_text: "",
             signup: "",
             digid: "Open het DigiD inlogscherm",
+            restore_title: "Account herstellen",
+            restore_header: "",
         }
     }
 };
