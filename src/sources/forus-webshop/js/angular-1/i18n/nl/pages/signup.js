d8a5250b35a8b30bf155b03359d64ec715f7d3cb
Gerben <gerben@forus.io>
2022-12-21 00:11:08 +0100

    added translation

---
+++ src/sources/forus-webshop/js/angular-1/i18n/nl/pages/signup.js
@@ -0,0 +1,20 @@
+module.exports = {
+    // SIGNUP = sign-up.pug
+    items: {
+        title: "Aanmelden",
+        header: "Hoe wilt u aanmelden?",
+        pane_text: "Account herstellen of met een andere optie inloggen?",
+        digid: "wordt doorverwezen naar DigiD om aan te melden",
+        restore_title: "Account herstellen",
+        restore_header: "",
+        ede: {
+            title: "Inloggen",
+            header: "Hier logt u in",
+            pane_text: "",
+            signup: "",
+            digid: "Open het DigiD inlogscherm",
+            restore_title: "Account herstellen",
+            restore_header: "",
+        }
+    }
+};
