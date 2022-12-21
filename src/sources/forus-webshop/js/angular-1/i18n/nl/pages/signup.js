6a50882d00783a6422e3fd3af5724db54395dcb8
Gerben <gerben@forus.io>
2022-12-21 11:10:27 +0100

    added translations based on implementation for signuplfows

--- src/sources/forus-webshop/js/angular-1/i18n/nl/pages/signup.js
+++ src/sources/forus-webshop/js/angular-1/i18n/nl/pages/signup.js
@@ -7,6 +7,7 @@ module.exports = {
         digid: "Open het DigiD inlogscherm",
         restore_title: "Account herstellen",
         restore_header: "",
+        start_email: "Start aanvraag",
         ede: {
             title: "Inloggen",
             header: "Hier logt u in",
@@ -15,6 +16,25 @@ module.exports = {
             digid: "Open het DigiD inlogscherm",
             restore_title: "Account herstellen",
             restore_header: "",
-        }
+        },
+        vergoedingen: {
+            title: "Inloggen",
+            restore_title: "Account herstellen",
+        },
+        oostgelre: {
+            start_email: "Start activatie",
+        },
+        winterswijk: {
+            start_email: "Start activatie",
+        },
+        berkelland: {
+            start_email: "Start activatie",
+        },
+        noordoostpolder: {
+            start_email: "Start activatie",
+        },
+        heumen: {
+            start_email: "Start activatie",
+        },
     }
 };
