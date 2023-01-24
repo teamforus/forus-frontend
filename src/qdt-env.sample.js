5f3fd773c623a8a44269767a9cfae3f96d198511
dev-rminds <dev@rminds.nl>
2023-01-24 10:35:28 +0200

    product details page refactring

--- src/qdt-env.sample.js
+++ src/qdt-env.sample.js
@@ -751,6 +751,7 @@ module.exports = (core) => {
             client_key: 'ede',
             matomo_site_id: false,
             flags: {
+                logoExtension: '.png',
                 showAccountSidebar: false,
                 accessibilityPage: true,
                 genericSearch: true,
@@ -763,7 +764,7 @@ module.exports = (core) => {
 
                 // voucher settings
                 shareProducts: false,
-                product_details_only_available_funds: true
+                productDetailsOnlyAvailableFunds: true,
             },
             sessions: sessions,
             google_maps_api_key: google_maps_api_key,
