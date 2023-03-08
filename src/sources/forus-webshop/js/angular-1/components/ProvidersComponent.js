5d08ed2f8463adbb3c008d4c84fe7cc2bfc08bf2
dev-rminds <dev@rminds.nl>
2023-03-08 17:56:12 +0200

    add flag to show only used product categories on webshop

--- src/sources/forus-webshop/js/angular-1/components/ProvidersComponent.js
+++ src/sources/forus-webshop/js/angular-1/components/ProvidersComponent.js
@@ -1,5 +1,6 @@
 const ProvidersComponent = function (
     $state,
+    appConfigs,
     $stateParams,
     ProvidersService,
     FormBuilderService,
@@ -132,7 +133,9 @@ const ProvidersComponent = function (
         if (type === 'category') {
             if ($ctrl.product_category_id) {
                 ProductCategoryService.list({
-                    parent_id: $ctrl.product_category_id, per_page: 1000, used: 1,
+                    parent_id: $ctrl.product_category_id, 
+                    used: appConfigs.flags.showOnlyUsedCategories ? 1 : null,
+                    per_page: 1000, 
                 }).then(res => {
                     $ctrl.productSubCategories = res.data.meta.total ? [{
                         name: 'Selecteer subcategorie...',
@@ -214,6 +217,7 @@ const ProvidersComponent = function (
         }
 
         $ctrl.updateFiltersUsedCount();
+        console.log(appConfigs);
     };
 };
 
@@ -228,6 +232,7 @@ module.exports = {
     },
     controller: [
         '$state',
+        'appConfigs',
         '$stateParams',
         'ProvidersService',
         'FormBuilderService',
