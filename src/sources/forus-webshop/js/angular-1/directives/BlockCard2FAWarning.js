e970a9f0f07ea0491494d42c018c32c50b2a5245
dev-rminds <dev@rminds.nl>
2023-08-03 14:58:17 +0300

    fund 2fa warning block refactoring

--- src/sources/forus-webshop/js/angular-1/directives/BlockCard2FAWarning.js
+++ src/sources/forus-webshop/js/angular-1/directives/BlockCard2FAWarning.js
@@ -1,28 +1,14 @@
-const BlockCard2FAWarning = function($scope) {
+const BlockCard2FAWarning = function ($scope) {
     const $dir = $scope.$dir;
-    
-    $dir.setShowMore2FADetails = (e, showMore2FADetails = false) => {
-        e?.preventDefault();
-        e?.stopPropagation();
-        
-        $dir.showMore2FADetails = showMore2FADetails;
-    };
 
     $dir.$onInit = () => {
-        $dir.expandButtonPos = $dir.expandButtonPos || 'top';
+        $dir.settings = $dir.fund.auth_2fa_policy == 'global' ? $dir.fund.organization_funds_2fa : $dir.fund;
 
-        const global2Fa = $dir.fund.auth_2fa_policy == 'global';
-        const prefix2FA = global2Fa ? 'funds_' : '';
-        const settings2FA = global2Fa ? $dir.fund.organization_funds_2fa : $dir.fund;
-        
-        $dir.policy = settings2FA[`auth_2fa_${prefix2FA}policy`];
-
-        if ($dir.policy == 'restrict_features') {
-            $dir.restrictEmails = settings2FA[`auth_2fa_${prefix2FA}restrict_emails`];
-            $dir.restrictSessions = settings2FA[`auth_2fa_${prefix2FA}restrict_auth_sessions`];
-            $dir.restrictReimbursements = settings2FA[`auth_2fa_${prefix2FA}restrict_reimbursements`];
-            $dir.hasRestrictions = $dir.restrictEmails || $dir.restrictSessions || $dir.restrictReimbursements;
-        }
+        $dir.hasRestrictions = $dir.settings.auth_2fa_policy == 'restrict_features' && (
+            $dir.settings.auth_2fa_restrict_emails ||
+            $dir.settings.auth_2fa_restrict_auth_sessions ||
+            $dir.settings.auth_2fa_restrict_reimbursements
+        );
     };
 };
 
@@ -30,7 +16,7 @@ module.exports = () => {
     return {
         scope: {
             fund: '=',
-            expandButtonPos: '@',
+            buttonPosition: '@',
         },
         replace: false,
         bindToController: true,
@@ -40,6 +26,6 @@ module.exports = () => {
             '$scope',
             BlockCard2FAWarning,
         ],
-        templateUrl: 'assets/tpl/directives/block-card-2fa-warning.html'
+        templateUrl: 'assets/tpl/directives/block-card-2fa-warning.html',
     };
 };
