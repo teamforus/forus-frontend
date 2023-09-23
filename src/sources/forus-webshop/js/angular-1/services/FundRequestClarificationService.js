e5ed3e46f584fb700611bef4d91b89f985a1c14c
Yaroslav Kosterin <ateamcms@gmail.com>
2023-09-23 12:25:44 +0300

    add fund requests for requesters on webshop

--- src/sources/forus-webshop/js/angular-1/services/FundRequestClarificationService.js
+++ src/sources/forus-webshop/js/angular-1/services/FundRequestClarificationService.js
@@ -4,14 +4,6 @@ let FundRequestClarificationService = function(ApiRequest) {
     let uriPrefix = '/platform/funds/%s/requests/%s/clarifications';
 
     return new(function() {
-        this.index = function(fund_id, request_id, data = {}) {
-            return ApiRequest.get(sprintf(uriPrefix, fund_id, request_id), data);
-        };
-
-        this.read = function(fund_id, request_id, id, data = {}) {
-            return ApiRequest.get(sprintf(uriPrefix + '/%s', fund_id, request_id, id), data);
-        };
-
         this.update = function(fund_id, request_id, id, data = {}) {
             return ApiRequest.patch(sprintf(uriPrefix + '/%s', fund_id, request_id, id), data);
         };
