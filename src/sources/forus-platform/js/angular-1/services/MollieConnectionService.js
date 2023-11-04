e1e09775d916450703027351d562ad3b0c9944f1
dev-rminds <dev@rminds.nl>
2023-11-04 18:44:56 +0200

    mollie integration initial refactoring

--- src/sources/forus-platform/js/angular-1/services/MollieConnectionService.js
+++ src/sources/forus-platform/js/angular-1/services/MollieConnectionService.js
@@ -1,40 +1,38 @@
 const MollieConnectionService = function(ApiRequest) {
-    return new (function() {
-        this.getConfigured = function(organization_id, query = {}) {
-            return ApiRequest.get(`/platform/organizations/${organization_id}/mollie-connections/configured`, query);
-        };
+    const prefix = '/platform/organizations';
 
+    return new (function() {
         this.store = function(organization_id, data) {
-            return ApiRequest.post(`/platform/organizations/${organization_id}/mollie-connections`, data);
+            return ApiRequest.post(`${prefix}/${organization_id}/mollie-connections`, data);
         };
 
         this.connect = function(organization_id) {
-            return ApiRequest.get(`/platform/organizations/${organization_id}/mollie-connections/connect`);
+            return ApiRequest.post(`${prefix}/${organization_id}/mollie-connections/connect`);
+        };
+
+        this.getConfigured = function(organization_id, query = {}) {
+            return ApiRequest.get(`${prefix}/${organization_id}/mollie-connections/configured`, query);
         };
 
         this.fetch = function(organization_id) {
-            return ApiRequest.get(`/platform/organizations/${organization_id}/mollie-connections/fetch`);
+            return ApiRequest.get(`${prefix}/${organization_id}/mollie-connections/fetch`);
         };
 
         this.destroy = function (organization_id, id) {
-            return ApiRequest.delete(`/platform/organizations/${organization_id}/mollie-connections/${id}`);
-        }
-
-        this.updateProfile = function (organization_id, id, profile_id, data) {
-            return ApiRequest.patch(
-                `/platform/organizations/${organization_id}/mollie-connections/${id}/profiles/${profile_id}`, data
-            );
+            return ApiRequest.delete(`${prefix}/${organization_id}/mollie-connections/${id}`);
         }
 
         this.storeProfile = function (organization_id, id, data) {
-            return ApiRequest.post(
-                `/platform/organizations/${organization_id}/mollie-connections/${id}/profiles`, data
-            );
+            return ApiRequest.post(`${prefix}/${organization_id}/mollie-connections/${id}/profiles`, data);
+        }
+
+        this.updateProfile = function (organization_id, connection_id, id, data) {
+            return ApiRequest.patch(`${prefix}/${organization_id}/mollie-connections/${connection_id}/profiles/${id}`, data);
         }
     });
 };
 
 module.exports = [
     'ApiRequest',
-    MollieConnectionService
+    MollieConnectionService,
 ];
