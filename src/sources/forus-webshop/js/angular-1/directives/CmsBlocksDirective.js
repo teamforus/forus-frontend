75c53a996f55d4922c7f50bdb1e0c3af745b3752
aghimpu <aghimpu90@gmail.com>
2023-11-22 12:37:55 +0200

    fix cms blocks layout issue

--- src/sources/forus-webshop/js/angular-1/directives/CmsBlocksDirective.js
+++ src/sources/forus-webshop/js/angular-1/directives/CmsBlocksDirective.js
@@ -18,7 +18,6 @@ module.exports = () => {
     return {
         scope: {
             page: '=',
-            noWrapper: '=',
         },
         bindToController: true,
         controllerAs: '$dir',
