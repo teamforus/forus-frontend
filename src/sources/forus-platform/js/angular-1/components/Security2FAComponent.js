e4eaefd182949d81e11ccc5dc7a403a3be186708
aghimpu <aghimpu90@gmail.com>
2024-01-17 22:38:57 +0200

    add mask to 2fa phone number

--- src/sources/forus-platform/js/angular-1/components/Security2FAComponent.js
+++ src/sources/forus-platform/js/angular-1/components/Security2FAComponent.js
@@ -47,7 +47,12 @@ const Security2FAComponent = function (
         $ctrl.active_providers = active_providers;
         $ctrl.providers = providers;
         $ctrl.provider_types = provider_types;
-        $ctrl.active_provider_types = active_providers.map((auth_2fa) => auth_2fa.provider_type.type)
+        $ctrl.active_provider_types = active_providers.map((auth_2fa) => auth_2fa.provider_type.type);
+
+        $ctrl.active_providers = $ctrl.provider_types.reduce((list, item) => ({ 
+            ...list, 
+            [item.type]: active_providers.find((auth_2fa) => auth_2fa.provider_type.type == item.type),
+        }), {});
 
         $ctrl.auth2FARememberIpOptions = auth2FARememberIpOptions;
 
