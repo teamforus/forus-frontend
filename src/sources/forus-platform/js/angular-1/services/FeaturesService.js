16838991fa24371976273b88784983d6d943f5b1
MariaForus <maria@forus.io>
2024-01-29 15:50:22 +0100

    Text changes - overview page

--- src/sources/forus-platform/js/angular-1/services/FeaturesService.js
+++ src/sources/forus-platform/js/angular-1/services/FeaturesService.js
@@ -5,64 +5,64 @@ const FeaturesService = function () {
             name: 'BI-tool API',
             description: 'API-koppeling met Business Intelligence (BI) tools voor data-export.',
             overview_description: 'Maak gebruik van de API-koppeling met Business Intelligence (BI) tools om gegevens te exporteren.',
-            labels: ['Koppeling', 'Business Intelligence', 'Management'],
+            labels: ['Integratie', 'Managementinformatie', 'API'],
         }, {
             key: 'email_connection',
             name: 'E-mailkoppeling',
             description: 'Mogelijkheid om systeemgerelateerde e-mails te versturen vanuit een gepersonaliseerd adres van uw organisatie.',
             overview_description: 'Verstuur systeemgerelateerde e-mails vanuit een gepersonaliseerd adres van uw organisatie.',
-            labels: ['E-mail', 'Communicatie', 'Beveiliging'],
+            labels: ['Integratie', 'Communicatie', 'Beveiliging'],
         }, {
             key: 'backoffice_api',
             name: 'Backoffice API',
             description: 'Koppeling met de gemeente-API voor naadloze gegevensintegratie.',
             overview_description: 'Integreer gegevens met de gemeente-API.',
-            labels: ['Integratie', 'Gegevens', 'Gemeente API'],
+            labels: ['Integratie', 'Managementinformatie', 'Aanvragen', 'API'],
         }, {
             key: 'iconnect_api',
             name: 'Haal Centraal API',
             description: 'Toegang tot BRP-gegevens en weergave in het Forus platform.',
             overview_description: 'Verkrijg toegang tot BRP-gegevens en bekijk ze in het Forus-platform.',
-            labels: ['Integratie', 'Gegevens', 'BRP', 'API'],
+            labels: ['Integratie', 'Persoonsgegevens', 'Aanvragen', 'API'],
         }, {
             key: 'digid',
             name: 'DigiD',
             description: 'Een koppeling met DigiD voor digitale identificatie binnen Forus.',
             overview_description: 'Faciliteer het proces voor deelnemers: laat deelnemers inloggen en fondsen aanvragen met DigiD.',
-            labels: ['Koppeling', 'Beveiliging', 'Identificatie'],
+            labels: ['Integratie', 'Beveiliging', 'Aanvragen', 'Identificatie'],
         }, {
             key: 'bng',
             name: 'BNG',
             description: 'Geautomatiseerde financiële transacties tussen Forus en de rekening van uw organisatie bij de Bank Nederlandse Gemeenten (BNG).',
             overview_description: 'Maak gebruik van geautomatiseerde financiële transacties tussen Forus en uw ' +
                 'organisatierekening bij de Bank Nederlandse Gemeenten (BNG).',
-            labels: ['Koppeling', 'Financieel'],
+            labels: ['Integratie', 'Financieel'],
         }, {
             key: 'reimbursements',
             name: 'Declaraties',
             description: 'Mogelijkheid voor deelnemers om bonnen en facturen in te dienen ter vergoeding van kosten.',
             overview_description: 'Faciliteer deelnemers bij het indienen van bonnen en facturen ter vergoeding van kosten.',
-            labels: ['Declaraties', 'Kostenvergoeding', 'Beoordeling'],
+            labels: ['Toegankelijkheid', 'Financieel'],
         }, {
             key: 'auth_2_fa',
             name: 'Tweefactorauthenticatie (2FA)',
             description: 'Extra beveiligingslaag voor accountbescherming.',
             overview_description: 'Voeg een extra beveiligingslaag toe voor de bescherming van uw account.',
-            labels: ['Beveiliging', 'Accountbescherming', '2FA'],
+            labels: ['Beveiliging', 'Identificatie'],
         }, {
             key: 'physical_cards',
             name: 'Fysieke pas',
             description: 'Aanvragen en uitgifte van een fysieke pas met persoonlijke QR.',
             overview_description: 'Geef minder digitaal vaardige deelnemers een alternatief voor digitale toegang.',
-            labels: ['Fysieke pas', 'Stadspas', 'Minder digitaal vaardigen'],
+            labels: ['Toegankelijkheid'],
         }];
 
         this.disabled = [{
             key: 'voucher_records',
-            name: 'Eigenschappen op voucher',
-            description: 'Voeg persoonlijke eigenschappen toe en exporteer ze bij vouchers.',
-            overview_description: ' Voeg persoonlijke eigenschappen toe en exporteer ze bij vouchers.',
-            labels: ['Gebruiksvriendelijkheid'],
+            name: 'Persoonsgegevens op een tegoed',
+            description: 'Het toevoegen en tonen van persoonsgegevens op een tegoed.',
+            overview_description: ' Voeg persoonsgegevens toe en toon ze op een tegoed.',
+            labels: ['Beveiliging', 'Persoonsgegevens', 'Identificatie'],
         }];
 
         this.previewList = [[{
